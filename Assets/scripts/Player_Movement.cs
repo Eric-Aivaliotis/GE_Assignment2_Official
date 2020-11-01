5f9ce58f84b23acc5ca061388a7b91a1eca180aa
Eric-Aivaliotis <55474867+Eric-Aivaliotis@users.noreply.github.com>
2020-11-01 00:34:36 -0400

    Add files via upload

---
+++ Assets/scripts/Player_Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public float player_speed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-player_speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f, player_speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+    }
+}
